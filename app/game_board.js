cf8ad4552d69912a1fc5d157f03164dabfeaad68
Cristian <cristianariel375@gmail.com>
2026-01-23 22:12:08 -0300

    feat-test(gameBoard): add reveal box method

--- app/game_board.js
+++ app/game_board.js
@@ -22,38 +22,9 @@ export class GameBoard {
         return [...this.#board]
     }
 
-    // #createBoardv2(){
-    //     if (this.#totalMines > this.#rows*this.#cols){
-    //         throw Error('Number of mines must be less than total number of cells.')
-    //     }
-    //     const board = []
-    //     const randomIndex = []
-    //     // Genero aleatoriamente los índices que tendrán minas 
-    //     while (randomIndex.length < this.#totalMines){
-    //         let rand_i = getRandomIntInclusive(0, this.#rows - 1)
-    //         let rand_j = getRandomIntInclusive(0, this.#cols - 1)
-    //         if (!randomIndex.find(index => index[0]==rand_i & index[1]==rand_j)){
-    //             randomIndex.push([rand_i, rand_j])
-    //         }
-    //     }
-    //     // Generamos la tabla
-    //     let row, box, isMine
-    //     for (let i = 0; i < this.#rows; i++){
-    //         row = []
-    //         for (let j = 0; j < this.#cols; j++){
-    //             isMine = false
-    //             // Si toca combinacion de indices de una mina
-    //             //if (randomIndex.includes([i, j])){
-    //             if (randomIndex.find(index => index[0]==i & index[1]==j)){
-    //                 isMine = true
-    //             }
-    //             box = new Box(isMine)
-    //             row.push(box)
-    //         }
-    //         board.push(row)
-    //     }
-    //     return board
-    // }
+    get totalMines(){
+        return this.#totalMines
+    }
 
     #createBoard(){
         const board = []
@@ -69,11 +40,19 @@ export class GameBoard {
         return board
     }
 
-    setMineIn(i, j){
-        if (!((i >= 0 && i < this.#rows) && (j >= 0 && j < this.#cols))){
+    getBoxIn(i, j){
+        if ((i < 0 || i >= this.#rows) || (j < 0 || j >= this.#cols) ){
             throw Error(`Index out of range: [${i}, ${j}]`)
         }
-        const box = this.#board[i][j]
+        return this.#board[i][j]
+    }
+
+    setMineIn(i, j){
+        // if (!((i >= 0 && i < this.#rows) && (j >= 0 && j < this.#cols))){
+        //     throw Error(`Index out of range: [${i}, ${j}]`)
+        // }
+        // const box = this.#board[i][j]
+        const box = this.getBoxIn(i, j)
         if (!box.is_mine){
             box.is_mine = true
             this.#totalMines++
@@ -97,27 +76,14 @@ export class GameBoard {
         }
     }
 
-    get totalMines(){
-        return this.#totalMines
-    }
-
     revealBox(i, j){
-        if ((i < 0 || i >= this.#rows) || (j < 0 || j >= this.#cols) ){
-            throw Error(`Index out of range: [${i}, ${j}]`)
-        }
-        let box = this.#board[i][j]
+        let box = this.getBoxIn(i, j)
         if (box.reveal()){
             if (!box.is_mine){
                 // Check neighboring cells for counting bombs
                 let sideCells = this.getNeighbours(i, j)
                 // Nos quedamos con aquellas aún no reveladas. Necesario para limitar la recursión
                 sideCells = sideCells.filter( box => !box.revealed)
-                //let bombsCount = 0 // Cambiar por un atributo de una mina
-                // for (let k = 0; k<=sideCells.length; k++){
-                //     if (sideCells[k].is_mine){
-                //         bombsCount++
-                //     }
-                // }
                 box.nearby_mines = 0
                 sideCells.forEach( cell => {
                     if(cell.is_mine){
@@ -129,8 +95,8 @@ export class GameBoard {
                     sideCells.forEach( cell  => this.revealBox(...cell.position))
                 }
             }else{
-                // Lose Game
-                throw Error('Bomba, perdiste el juego...')
+                // Lost Game
+                throw Error('Lost Game')
             }
         }
     }
@@ -147,7 +113,7 @@ export class GameBoard {
                 }
                 try{
                     // Le agrego una propiedad a la instancia del objeto
-                    let sideCell = this.#board[i+di][j+dj]
+                    let sideCell = this.getBoxIn(i+di, j+dj)
                     sideCell.position = [i+di, j+dj]
                     cells.push(sideCell)
                 }catch{
@@ -156,10 +122,6 @@ export class GameBoard {
             }
         }
         return cells
-    }
-
-    showBoard(){
-        console.log(this.#board)
     }
 }
 
--- tests/game_board.test.js
+++ tests/game_board.test.js
@@ -12,10 +12,10 @@ describe('Test Game Board', () => {
     // });
 
     test('Shape Board', () => {
-        const board = new GameBoard(8, 8).board
-        expect(board.length).toBe(8)
-        board.forEach(row => expect(row.length).toBe(8))
-    })
+        const gameBoard = new GameBoard(8, 8).board
+        expect(gameBoard.length).toBe(8)
+        gameBoard.forEach(row => expect(row.length).toBe(8))
+    });
 
     test('El Tablero debe ser un Array, que debe contener Arrays de objetos del tipo Box', () => {
         const board = new GameBoard(8, 8).board
@@ -28,11 +28,17 @@ describe('Test Game Board', () => {
         })
     });
 
+    test('Obtener celda en una posición específica', () => {
+        const gameBoard = new GameBoard(5, 5)
+        expect(gameBoard.getBoxIn(0, 0)).toBeInstanceOf(Box)
+        expect(() => gameBoard.getBoxIn(-1, -1).toThrow(Error))
+    })
+
     test('Setear una mina en una posición específica', () => {
         const gameBoard = new GameBoard(8, 8)
         gameBoard.setMineIn(1, 1)
         expect(gameBoard.board[1][1].is_mine).toBeTruthy()
-    })
+    });
 
     test('El Tablero debe contener el número determinado de minas', () => {
         let totalMines = 20
@@ -50,21 +56,81 @@ describe('Test Game Board', () => {
             })
         })
         expect(mines).toBe(totalMines)
-    })
+    });
 
     test('Error si el número total de minas aleatorias no es válido', () => {
         const gameBoard = new GameBoard(8, 8)
         totalMines = 8*8 + 1
         expect(() => gameBoard.setRandomMines(totalMines)).toThrow(Error);
-    })
+    });
 
-    // test('Revelar casilla', () => {
-    //     const gameBoard = new GameBoard(5, 5, 1)
-    //     const board = gameBoard.board
-    //     const minePosition = board.find( row => row.find( box => box.is_mine ))
-    //     console.log(minePosition)
-    //     const i = board.findIndex( row => row.find( box => box.is_mine ))
-    //     const j = board[j].findIndex( box => box.is_mine)
-    //     // Testear excepción de juego perdido
-    // })
+    test('Obtener vecinos', () => {
+        const gameBoard = new GameBoard(5, 5)
+        const neighbours = gameBoard.getNeighbours(2, 2)
+        // Deben ser Array con objetos Box
+        expect(neighbours).toEqual(expect.arrayOf(expect.any(Box)))
+        expect(neighbours.length).toBe(8)
+        // Una celda en un borde debe tener 5 vecinos
+        expect(gameBoard.getNeighbours(2,0).length).toBe(5)
+        // Una celda en una esquina debe tener 3 vecinos
+        expect(gameBoard.getNeighbours(0,0).length).toBe(3)
+        // Excepción indice fuera de rango
+        expect(() => expect(gameBoard.getNeighbours(-1,-1))).toThrow(Error)
+    });
+
+    describe('Reveal Box feat', () => {
+        test('Revelar casilla: Sin minas. Debe revelarse el tablero completo', () => {
+            const gameBoard = new GameBoard(5, 5)
+            gameBoard.revealBox(0, 0)
+            // Todas las casillas deben haber sido reveladas
+            gameBoard.board.forEach( row => {
+                row.forEach( box => {
+                    expect(box.revealed).toBeTruthy()
+                })
+            })
+        });
+
+        test('Revelar casilla: 1 mina. Vecinos no deben revelarse y en total haber solo una casilla revelada', () => {
+            const minePosition = [2,2]
+            const boxToReveal = [minePosition[0]+1, minePosition[1]+1]
+            const gameBoard = new GameBoard(5, 5)
+            gameBoard.setMineIn(...minePosition)
+            gameBoard.revealBox(...boxToReveal)
+            expect(gameBoard.getBoxIn(...boxToReveal).revealed).toBeTruthy()
+            // Sus vecinos no deben haber sido revelados puesto que hay una mina en ellos
+            const neighbours = gameBoard.getNeighbours(...boxToReveal)
+            neighbours.forEach(box => expect(box.revealed).toBeFalsy())
+            // Solo debe haber una casilla revelada en toda la matriz
+            const board = gameBoard.board
+            let revealedMines = 0
+            board.forEach(row => row.forEach( box => {
+                if (box.revealed){
+                    revealedMines++
+                }
+            }))
+            expect(revealedMines).toBe(1)
+        });
+
+        test('Revelar casilla: 1 mina. Caso recursion', () => {
+            const gameBoard = new GameBoard(5, 5)
+            const minePosition = [0, 0]
+            gameBoard.setMineIn(...minePosition)
+            gameBoard.revealBox(4, 4)
+            // Deben haberse revelado todas las casillas en cascada, exceptuando la única mina
+            expect(gameBoard.getBoxIn(...minePosition).revealed).toBeFalsy()
+            const board = gameBoard.board
+            board.forEach( row => row.forEach(box => expect(box.revealed).toBeTruthy))
+        });
+
+        test('Revelar casilla fuera de rango', () => {
+            const gameBoard = new GameBoard(5, 5)
+            expect(() => gameBoard.revealBox(-1, -1)).toThrow(Error)
+        });
+
+        test('Revelar casilla con minal. Juego perdido', () => {
+            const gameBoard = new GameBoard(5, 5)
+            gameBoard.setMineIn(0,0)
+            expect(() => gameBoard.revealBox(0,0)).toThrow('Lost Game')
+        })
+    });
 })
